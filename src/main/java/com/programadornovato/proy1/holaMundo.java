2b94fea2ab7f3f98d4fe5ffe5f9d77b9f75261e4
Programador Novato <info@programadornovato.com>
2019-08-21 15:16:21 -0500

    Curso de Java Netbeans Completo☕ [09.- Operadores matemáticos]
    Video:
    https://youtu.be/bjLx0bhm5eQ

--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -18,9 +18,21 @@ public class holaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        char caracter= JOptionPane.showInputDialog("Mete un caracter").charAt(0);
-        JOptionPane.showMessageDialog(null,"Este es tu caracter "+caracter);
-
+        Scanner entra=new Scanner(System.in);
+        float num1,num2,suma,resta,mul,div,residuo;
+        System.out.println("Inresa 2 numeros:");
+        num1=entra.nextFloat();
+        num2=entra.nextFloat();
+        suma=num1+num2;
+        resta=num1-num2;
+        mul=num1*num2;
+        div=num1/num2;
+        residuo=num1%num2;
+        System.out.println("Suma="+suma);
+        System.out.println("Resta="+resta);
+        System.out.println("Multiplicacion="+mul);
+        System.out.println("Divicion="+div);
+        System.out.println("Residuo="+residuo);
     }
     
 }
