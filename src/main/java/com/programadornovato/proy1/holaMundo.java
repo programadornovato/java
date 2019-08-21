69167341ffd31fdd80775e5b16b4d090d75824ed
Programador Novato <info@programadornovato.com>
2019-08-21 14:07:08 -0500

    Curso de Java Netbeans Completo☕ [08.- Ingresar datos via showInputDialog]
    Video:
    https://youtu.be/sveCLOl_2dE

--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -6,6 +6,7 @@
 package com.programadornovato.proy1;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -17,33 +18,9 @@ public class holaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner inInt=new Scanner (System.in);
-        int entero;
-        System.out.println("Escriba un numero entero");
-        entero=inInt.nextInt();
-        System.out.println("Su numero es: "+entero);
-        
-        Scanner inFloat=new Scanner (System.in);
-        float flotante;
-        System.out.println("Escriba un numero flotante");
-        flotante=inFloat.nextFloat();
-        System.out.println("Su numero es: "+flotante);
-        
-        Scanner inCadena=new Scanner (System.in);
-        String cadena;
-        System.out.println("Escriba una cadena");
-        cadena=inCadena.nextLine();
-        System.out.println("Su cadena es: "+cadena);
-        
-        Scanner inCaracter=new Scanner (System.in);
-        char caracter;
-        System.out.println("Escriba un caracter");
-        caracter=inCaracter.next().charAt(0);
-        System.out.println("Su caracter es: "+caracter);
-        
-        
-        
+        char caracter= JOptionPane.showInputDialog("Mete un caracter").charAt(0);
+        JOptionPane.showMessageDialog(null,"Este es tu caracter "+caracter);
+
     }
     
 }
