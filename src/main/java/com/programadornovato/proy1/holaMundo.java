e808f25248e2a407a340509bd29d8f06e2e4a9a7
Programador Novato <info@programadornovato.com>
2019-08-26 01:36:33 -0500

    Curso de Java Netbeans Completo☕ [18.- Condicionales(switch case)]
    Video:
    https://youtu.be/fhbFaoHivVo

--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -21,15 +21,22 @@ public class holaMundo {
      */
     public static void main(String[] args) {
         Scanner entra=new Scanner(System.in);
-        int numUser,numSis;
-        numSis=(int)(Math.random()*10);
-        System.out.print("Ingresa un numero mayor o igual a "+numSis+": ");
-        numUser=entra.nextInt();
-        if(numUser >= numSis){
-            System.out.println("Muy bien");
-        }
-        else{
-            System.out.println("Muy mal");
+        System.out.println("Seleccione una opcion\n1: Acceso\n2: Configuracion\n3: Ayuda");
+        int seleleccion=entra.nextInt();
+        switch(seleleccion){
+            case 1:
+                System.out.println("Seleccionaste el accesos");
+                break;
+            case 2:
+                System.out.println("Seleccionaste la configuracion");
+                break;
+            case 3:
+                System.out.println("Seleccionaste la ayuda");
+                break;
+            default:
+                System.out.println("Opcion no valida");
+                break;
+
         }
     }
     
