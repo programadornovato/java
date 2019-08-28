1ef52d516a7bc82ee5c5516813c8a27782c696d7
Programador Novato <info@programadornovato.com>
2019-08-28 03:10:37 -0500

    Curso de Java Netbeans Completo☕ [25.- Ejercicio, Dibujar una escalera con asteriscos con ciclo for]
    Video:
    https://youtu.be/yBUmJpKCAkU

--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -21,28 +21,14 @@ public class holaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*
-        int i=1;
-        while(i<=10){
-            System.out.println("i="+i);
-            i=i+1;
+        String texto=JOptionPane.showInputDialog("Humano dame la altura de tu pinche escalera");
+        int altura=Integer.parseInt(texto);
+        for(int numAsterisco=1;numAsterisco<=altura;numAsterisco++){
+            for(int i=0;i<numAsterisco;i++){
+                System.out.print("*");
+            }
+            System.out.println("");
         }
-        
-        for(int i=1;i<=10;i++){
-            System.out.println("i="+i);
-        }
-
-        for(int i=10;i>=1;i--){
-            System.out.println("i="+i);
-        }
-        */
-        Scanner entra=new Scanner(System.in);
-        System.out.println("Humano cuantas repeticiones quieres");
-        int repeticiones=entra.nextInt();
-        for(int i=1;i<=repeticiones;i++){
-            System.out.println("i="+i);
-        }
-
     }
     
 }
