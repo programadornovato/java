74888254f1fdc9c294c4996f85be21d4ab6a934e
Programador Novato <info@programadornovato.com>
2019-09-01 02:49:17 -0500

    Curso de Java Netbeans Completo☕ [38.- Busqueda binaria]
    Video:
    https://youtu.be/c33-zbeyx_U

--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -21,32 +21,39 @@ public class holaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //5 10 15 58 20 56 65
+        //5 10 15 30 50 70 90 95
         Scanner entra=new Scanner(System.in);
-        int elemento[]=new int[7];
-        System.out.println("Humano holgazan escribe 7 numeros");
-        for(int i=0;i<7;i++){
+        int elemento[]=new int[8];
+        System.out.println("Humano holgazan escribe 8 numeros");
+        for(int i=0;i<8;i++){
             elemento[i]=entra.nextInt();
         }
         
         System.out.println("Humano ahora ingresa tu piche numero a buscar");
         int numBuscar=entra.nextInt();
         
-        int i=0;
+        int n=elemento.length, inf=0,centro=0;
+        int sup=n-1;
         boolean bandera=false;
-        while(i<7 && bandera== false){
-            if(numBuscar==elemento[i]){
+        while(inf<=sup){
+            centro=(sup+inf)/2;
+            if(elemento[centro]==numBuscar){
                 bandera=true;
+                break;
             }
-            i++;
+            else if(numBuscar<elemento[centro]){
+                sup=centro-1;
+            }
+            else{
+                inf=centro+1;
+            }
         }
-        if(bandera){
-            System.out.println("Humano!!! ya encontre tu pinche numero en la posicion "+i);
+        if(bandera==true){
+            System.out.println("Humano ya encontre tu numero estaba en la pos "+(centro+1));
         }
         else{
-            System.out.println("Humano estupido!! no encontre tu numerito");
+            System.out.println("Lo siento Humano no encontre tu numerito");
         }
-        
 
     }
     
