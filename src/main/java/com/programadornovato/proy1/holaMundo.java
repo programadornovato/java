a913eb818aaacb2b2dc9dbb0722cbd37e7ae7e7a
Programador Novato <info@programadornovato.com>
2019-09-01 17:40:27 -0500

    Curso de Java Netbeans Completo☕ [40.- Que es una matriz (matrix)]
    Video:
    https://youtu.be/6OiGIi4A8Fo

--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -21,40 +21,32 @@ public class holaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //5 10 15 30 50 70 90 95
+        //5 20 30
+        //8 15 5
+        //54 5 2
+        //int matriz[][]={{5,20,30},{8,15,5},{54,5,2}};
         Scanner entra=new Scanner(System.in);
-        int elemento[]=new int[8];
-        System.out.println("Humano holgazan escribe 8 numeros");
-        for(int i=0;i<8;i++){
-            elemento[i]=entra.nextInt();
-        }
-        
-        System.out.println("Humano ahora ingresa tu piche numero a buscar");
-        int numBuscar=entra.nextInt();
-        
-        int n=elemento.length, inf=0,centro=0;
-        int sup=n-1;
-        boolean bandera=false;
-        while(inf<=sup){
-            centro=(sup+inf)/2;
-            if(elemento[centro]==numBuscar){
-                bandera=true;
-                break;
-            }
-            else if(numBuscar<elemento[centro]){
-                sup=centro-1;
-            }
-            else{
-                inf=centro+1;
-            }
-        }
-        if(bandera==true){
-            System.out.println("Humano ya encontre tu numero estaba en la pos "+(centro+1));
-        }
-        else{
-            System.out.println("Lo siento Humano no encontre tu numerito");
-        }
+        System.out.println("Humano cuantos registro tene tu matriz:");
+        int reg= entra.nextInt();
+        System.out.println("Humano cuantas columnas tene tu matriz:");
+        int col= entra.nextInt();
+        int matriz[][]=new int[reg][col];
 
+        for(int i=0;i<3;i++){
+            System.out.println("Humano ingresa 3 datos de la fila "+(i+1));
+            for(int j=0;j<3;j++){
+                matriz[i][j]=entra.nextInt();
+            }
+            
+        }
+        
+        System.out.println("\nHumano esta es tu matriz");
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(matriz[i][j]+",");
+            }
+            System.out.println("");
+        }
     }
     
 }
