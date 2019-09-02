3687dbb0d76d22ff792ec7e2ac72624cef12e85e
Programador Novato <info@programadornovato.com>
2019-09-02 03:23:00 -0500

    Curso de Java Netbeans Completo☕ [41.- Matriz de String]
    Video:
    https://youtu.be/NQlGe-nmgjM

--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -21,32 +21,37 @@ public class holaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //5 20 30
-        //8 15 5
-        //54 5 2
-        //int matriz[][]={{5,20,30},{8,15,5},{54,5,2}};
+        /*
+Maria	Ramirez	C
+Juan	Perez	B
+Ines	Montes	B
+Rene	Pacheco	B
+Elena	Morales	A
+Mario	Dias	C
+Sonia	Navarro	A
+        */
+        
+        String alumnos[][]=new String[7][3];
         Scanner entra=new Scanner(System.in);
-        System.out.println("Humano cuantos registro tene tu matriz:");
-        int reg= entra.nextInt();
-        System.out.println("Humano cuantas columnas tene tu matriz:");
-        int col= entra.nextInt();
-        int matriz[][]=new int[reg][col];
-
-        for(int i=0;i<3;i++){
-            System.out.println("Humano ingresa 3 datos de la fila "+(i+1));
+        for(int i=0;i<alumnos.length;i++){
+            System.out.println("Humano!!! ingresa los datos del alumno "+(i+1));
             for(int j=0;j<3;j++){
-                matriz[i][j]=entra.nextInt();
+                alumnos[i][j]=entra.next();
             }
-            
         }
         
-        System.out.println("\nHumano esta es tu matriz");
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                System.out.print(matriz[i][j]+",");
+        System.out.println("Humano!!! ¿Dime de que aula quieres ver tus alumnos?");
+        String aula=entra.next();
+        aula=aula.toLowerCase();
+        for(int i=0;i<alumnos.length;i++){
+            if(alumnos[i][2].toLowerCase().equals(aula)){
+                System.out.println("");
+                for(int j=0;j<3;j++){
+                    System.out.print(alumnos[i][j]+" ");
+                }
             }
-            System.out.println("");
         }
+        
     }
     
 }
