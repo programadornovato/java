a2487776e2e8169a41a0731be07477235475fa81
Programador Novato <info@programadornovato.com>
2019-09-02 04:45:32 -0500

    Curso de Java Netbeans Completo☕ [42.- Matriz traspuesta]
    Video:
    https://youtu.be/x4_DmI1bZUM

--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -22,34 +22,33 @@ public class holaMundo {
      */
     public static void main(String[] args) {
         /*
-Maria	Ramirez	C
-Juan	Perez	B
-Ines	Montes	B
-Rene	Pacheco	B
-Elena	Morales	A
-Mario	Dias	C
-Sonia	Navarro	A
+        1	2	3
+        4	5	6
+        7	8	9
         */
-        
-        String alumnos[][]=new String[7][3];
+        int matriz[][]=new int[3][3];
+        int matrizT[][]=new int[3][3];
         Scanner entra=new Scanner(System.in);
-        for(int i=0;i<alumnos.length;i++){
-            System.out.println("Humano!!! ingresa los datos del alumno "+(i+1));
+        
+        for(int i=0;i<3;i++){
+            System.out.println("Humano!!! ingresa los datos de la fila "+(i+1));
             for(int j=0;j<3;j++){
-                alumnos[i][j]=entra.next();
+                matriz[i][j]=entra.nextInt();
             }
         }
         
-        System.out.println("Humano!!! ¿Dime de que aula quieres ver tus alumnos?");
-        String aula=entra.next();
-        aula=aula.toLowerCase();
-        for(int i=0;i<alumnos.length;i++){
-            if(alumnos[i][2].toLowerCase().equals(aula)){
-                System.out.println("");
-                for(int j=0;j<3;j++){
-                    System.out.print(alumnos[i][j]+" ");
-                }
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                matrizT[j][i]=matriz[i][j];
             }
+        }
+        
+        System.out.println("Humano aqui esta tu pinc**%*%* matriz traspuesta");
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(matrizT[i][j]+"\t");
+            }
+            System.out.println("");
         }
         
     }
