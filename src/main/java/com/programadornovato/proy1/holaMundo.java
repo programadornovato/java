b027779087f23630b4ebdb32244ba8723a336b10
Programador Novato <info@programadornovato.com>
2019-09-02 22:53:33 -0500

    Curso de Java Netbeans Completo☕ [44.- Demostrar que una matriz es simétrica]
    Video:
    https://youtu.be/Gp7V8CE7Gm8

--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -22,43 +22,35 @@ public class holaMundo {
      */
     public static void main(String[] args) {
         Scanner entra=new Scanner(System.in);
-        System.out.println("Humano dime cuantas filas tiene tu matriz");
+        System.out.println("Humano!! cuantas filas va a tener tu inche matriz");
         int fil=entra.nextInt();
-        System.out.println("Humano dime cuantas columnas tiene tu matriz");
+        System.out.println("Humano!! cuantas columnas va a tener tu inche matriz");
         int col=entra.nextInt();
         int matriz[][]=new int[fil][col];
         
-        boolean esSimetrica=true;
         for(int i=0;i<fil;i++){
             System.out.println("Humano ingresa los datos de la fila "+(i+1));
             for(int j=0;j<col;j++){
                 matriz[i][j]=entra.nextInt();
             }
         }
-        
-        if(col==fil){
-            for(int i=0;i<fil;i++){
-                for(int j=0;j<col;j++){
-                    if(matriz[i][j]!=matriz[j][i]){
-                        esSimetrica=false;
-                        break;
-                    }
-                }
-                if(esSimetrica==false){
-                    break;
-                }
+        int sumaFil=0;
+        for(int i=0;i<fil;i++){
+            sumaFil=0;
+            for(int j=0;j<col;j++){
+                sumaFil=sumaFil+matriz[i][j];
             }
+            System.out.println("Humano esta es la suma de tu fila "+(i+1)+"="+sumaFil);
         }
-        else{
-            System.out.println("Humano tu matriz para empesar no es simetrica");
+        int sumaCol=0;
+        for(int j=0;j<col;j++){
+            sumaCol=0;
+            for(int i=0;i<fil;i++){
+                sumaCol=sumaCol+matriz[i][j];
+            }
+            System.out.println("Humano esta es la suma de tu columna "+(j+1)+"="+sumaCol);
         }
         
-        if(esSimetrica==true){
-            System.out.println("Humano felicidades tu matriz si es simetrica");
-        }
-        else{
-            System.out.println("Nop no es simetrica");
-        }
     }
     
 }
