3fd386e097ec56c2d1641c518ccbcd990530ae5b
Programador Novato <info@programadornovato.com>
2019-09-03 01:47:51 -0500

    Curso de Java Netbeans Completo☕ [45.- Suma de una Diagonal de nuestra Matriz]
    Video:
    https://youtu.be/lSEYS22g7K8

--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -21,35 +21,54 @@ public class holaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner entra=new Scanner(System.in);
-        System.out.println("Humano!! cuantas filas va a tener tu inche matriz");
-        int fil=entra.nextInt();
-        System.out.println("Humano!! cuantas columnas va a tener tu inche matriz");
-        int col=entra.nextInt();
-        int matriz[][]=new int[fil][col];
+        int matriz[][]=new int [5][5];
+        int dato=1;
+        for(int i=0;i<matriz.length;i++){
+            for(int j=0;j<matriz[i].length;j++){
+                matriz[i][j]=dato;
+                dato++;
+            }
+        }
         
-        for(int i=0;i<fil;i++){
-            System.out.println("Humano ingresa los datos de la fila "+(i+1));
-            for(int j=0;j<col;j++){
-                matriz[i][j]=entra.nextInt();
+        for(int i=0;i<matriz.length;i++){
+            for(int j=0;j<matriz[i].length;j++){
+                System.out.print("\t"+matriz[i][j]);
+            }
+            System.out.println("");
+        }
+        
+        int diagonalPrincipal[]=new int[matriz.length];
+        int diagonalSecundaria[]=new int[matriz.length];
+        
+        for(int i=0;i<matriz.length;i++){
+            for(int j=0;j<matriz[i].length;j++){
+                if(i==j){
+                    diagonalPrincipal[i]=matriz[i][j];
+                }
+                if( (i+j) == matriz.length-1){
+                    diagonalSecundaria[i]=matriz[i][j];
+                }
             }
         }
-        int sumaFil=0;
-        for(int i=0;i<fil;i++){
-            sumaFil=0;
-            for(int j=0;j<col;j++){
-                sumaFil=sumaFil+matriz[i][j];
-            }
-            System.out.println("Humano esta es la suma de tu fila "+(i+1)+"="+sumaFil);
+        
+        int suma=0;
+        System.out.println("\nHumano este es tu diagonal principal");
+        for(int elemento:diagonalPrincipal){
+            System.out.print("\t"+elemento);
+            suma=suma+elemento;
         }
-        int sumaCol=0;
-        for(int j=0;j<col;j++){
-            sumaCol=0;
-            for(int i=0;i<fil;i++){
-                sumaCol=sumaCol+matriz[i][j];
-            }
-            System.out.println("Humano esta es la suma de tu columna "+(j+1)+"="+sumaCol);
+        System.out.print(" = "+suma);
+        System.out.println("");
+        
+        suma=0;
+        System.out.println("\nHumano este es tu diagonal secundaria");
+        for(int elemento:diagonalSecundaria){
+            System.out.print("\t"+elemento);
+            suma=suma+elemento;
         }
+        System.out.print(" = "+suma);
+        System.out.println("");
+        
         
     }
     
