13a2dcfcd4bd2df9e8d9ffbc9002e8d7095ce4e5
Programador Novato <info@programadornovato.com>
2019-09-03 23:19:04 -0500

    Curso de Java Netbeans Completo☕ [46.- POO (Programación Orientada a Objetos) Mi primer clase]
    Video:
    https://youtu.be/nateyNwftPo

---
+++ src/main/java/com/programadornovato/proy1/auto.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.programadornovato.proy1;
+
+/**
+ *
+ * @author eugenio
+ */
+public class auto {
+	String marca;
+	String modelo;
+	String color;
+        public void enciende(){
+            System.out.println("Enciende run run");
+        }
+        public void acelera(){
+            System.out.println("Velocimetro 80km");
+        }
+        public void frenar(){
+            System.out.println("Velocimetro 0km");
+        }
+}
--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -21,54 +21,15 @@ public class holaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int matriz[][]=new int [5][5];
-        int dato=1;
-        for(int i=0;i<matriz.length;i++){
-            for(int j=0;j<matriz[i].length;j++){
-                matriz[i][j]=dato;
-                dato++;
-            }
-        }
+        auto vocho=new auto();
+        vocho.modelo="1980";
+        vocho.marca="vocho";
+        vocho.color="azul";
         
-        for(int i=0;i<matriz.length;i++){
-            for(int j=0;j<matriz[i].length;j++){
-                System.out.print("\t"+matriz[i][j]);
-            }
-            System.out.println("");
-        }
-        
-        int diagonalPrincipal[]=new int[matriz.length];
-        int diagonalSecundaria[]=new int[matriz.length];
-        
-        for(int i=0;i<matriz.length;i++){
-            for(int j=0;j<matriz[i].length;j++){
-                if(i==j){
-                    diagonalPrincipal[i]=matriz[i][j];
-                }
-                if( (i+j) == matriz.length-1){
-                    diagonalSecundaria[i]=matriz[i][j];
-                }
-            }
-        }
-        
-        int suma=0;
-        System.out.println("\nHumano este es tu diagonal principal");
-        for(int elemento:diagonalPrincipal){
-            System.out.print("\t"+elemento);
-            suma=suma+elemento;
-        }
-        System.out.print(" = "+suma);
-        System.out.println("");
-        
-        suma=0;
-        System.out.println("\nHumano este es tu diagonal secundaria");
-        for(int elemento:diagonalSecundaria){
-            System.out.print("\t"+elemento);
-            suma=suma+elemento;
-        }
-        System.out.print(" = "+suma);
-        System.out.println("");
-        
+        vocho.enciende();
+        vocho.acelera();
+        vocho.frenar();
+        System.out.println("Marca "+vocho.marca);
         
     }
     
