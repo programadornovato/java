a568ae30e81b145499e5de9d38146a4d9fbb8917
Programador Novato <info@programadornovato.com>
2019-09-11 01:46:27 -0500

    Curso de Java Netbeans Completo☕ [65.- Ejercicio de POO Área de un terreno des uniforme (part 3)]
    Video:
    https://youtu.be/9CbwMmAlUNk

--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -16,12 +16,34 @@ import javax.swing.JOptionPane;
  * @author eugenio
  */
 public class holaMundo {
-
+    static Scanner entra=new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        char respuesta;
+        int opcion;
+        do{
+            System.out.println("Humano que tipo de pedazo de terreno quieres ingresar?");
+            System.out.println("1: Triangulo");
+            System.out.println("2: Rectangulo");
+            opcion=entra.nextInt();
+            switch(opcion){
+                case 1:llenaTriangulo();
+                break;
+                case 2:llenaRectangulo();
+                break;
+            }
+            System.out.println("Humano desea meter mas pedazos de terreno a calcular? s/n");
+            respuesta=entra.next().charAt(0);
+        }while( respuesta=='s' || respuesta=='S' );
+    }
+
+    protected static void llenaTriangulo() {
+        System.out.println("tringulo");
+    }
+    protected static void llenaRectangulo() {
+        System.out.println("rectangulo");
     }
     
 }
