cd9094a2481ef05f9b5070718560a34c7f1ee46b
Programador Novato <info@programadornovato.com>
2019-09-12 01:10:13 -0500

    Curso de Java Netbeans Completo☕ [68.- Que es una excepcion en java]
    Video:
    https://youtu.be/Q-jRihwAFF4

--- src/main/java/com/programadornovato/proy1/holaMundo.java
+++ src/main/java/com/programadornovato/proy1/holaMundo.java
@@ -24,35 +24,16 @@ public class holaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //listaNormal();
-        listaDinamica();
-    }
-
-    private static void listaNormal() {
-        int numEle=Integer.parseInt(JOptionPane.showInputDialog("Humano cuantos elelemtnos vas a ingresar"));
-        String lista[]=new String[numEle];
-        for(int i=0;i<lista.length;i++){
-            lista[i]=new String();
-            lista[i]=JOptionPane.showInputDialog("Humano ingresa el valor del elemento "+(i+1));
-        }
-        System.out.println("Resultados de la lista normal");
-        for(int i=0;i<lista.length;i++){
-            System.out.println(lista[i]);
-        }
-    }
-
-    private static void listaDinamica() {
-        ArrayList <String> lista=new ArrayList<String>();
-        char respuesta;
-        do{
-            lista.add(JOptionPane.showInputDialog("Humano ingresa el valor"));
-            respuesta=JOptionPane.showInputDialog("Humano quieres meter mas elementos? s/n").charAt(0);
-        }while(respuesta=='s'  || respuesta=='S' );
-        
-        System.out.println("Resultados de la lista dinamica");
-        for(int i=0;i<lista.size();i++){
-            System.out.println(lista.get(i));
-        }
-
+        /*
+        int a,b;
+        a=5;
+        b=0;
+        int resultado=a/b;
+        System.out.println(resultado);
+        */
+        Scanner entra=new Scanner(System.in);
+        System.out.println("Humano quiero que introduscas un numero:");
+        int num=entra.nextInt();
+        System.out.println(num);
     }
 }
